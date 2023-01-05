002d7ec2da9de78d4dfceedc3d6c9157e2249555
Uzer Qureshi <uq0803@gmail.com>
2023-01-05 08:25:21 +0530

    one day left

---
+++ q2.py
@@ -0,0 +1,8 @@
+def small(a,b):
+    if a<b:
+        return a
+    else:
+        return b
+sum=lambda x,y:x+y
+diff=lambda x,y:x-y
+print()
---
+++ q3.py
@@ -0,0 +1,9 @@
+a=int(input ("Entre your Age :\n"))
+if a==18 :
+    print("You are  18 you can give the vote")
+elif a<18 :
+    print("You cant give the vote")
+elif a>18 :
+    print("You can give the vote you are greater than 18")
+else:
+    print("Enter your Proper Age ")
