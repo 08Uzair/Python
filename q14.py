a5bc0da6e580b5b65e0ebd23dd5209c56f3980a4
Uzer Qureshi <uq0803@gmail.com>
2023-01-19 17:35:38 +0530

    Get commit

---
+++ q14.py
@@ -0,0 +1,80 @@
+# # message="Hello!"
+# # print(len(message))
+# # index=0
+# # for i in message:
+# #     print("message [", index ,"]",i)
+# #     index+=1
+
+# # b=list(message)
+# # print(b)
+# # message="Python Programing"
+# # print("Hello".format(message))
+# str="PYTHON"
+# length=len(str)
+# print(length)
+
+
+str1="Python Programming"
+a=str1[:7]
+b=str1[7:]
+print(a)
+print(b)
+d=a.join(b)
+print(d)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # # tuple=(1,2,3)
+# # # tuple1=("Hello","Welcome")
+# # # print(tuple1[::-1])
+# # # if (list[tuple]==list[tuple1]):
+# # #     print("yes")
+# # # else:
+# # #     print("No")
+
+
+
+# str="Pyhton"
+# b=len(str)
+# for i  in len(str):  # This will give error
+#     print(i)
+
+# a="nvjfvjhbcjk"
+# b="nnjbj,bc"
+# print(a.join(b))
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,17 +1,3 @@
-# #Write a Program to Change a Given String where first and last Character have been Exchanged 
-# str="Program"
-# a=slice(str[0])
-# b=slice(str[1:6])
-# c=slice(str[-1])
-# print(str[-1]+str[1:6]+str[0])
-# def interchange_chr(str1):
-#     return new_str[-1:]+
-def fibonacci(n):
-    a, b, c = 0, 1, 0
-    for i in range(n):
-        c = a + b
-        print(c)
-        a = b
-        b = c
-
-fibonacci(10)
+a="nvjfvjhbcjk"
+# b="nnjbj,bc"
+# print(a.join(b))
