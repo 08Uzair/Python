6f61ce9ecf779d1ab1d9ae191a27da2b395ec242
Uzer Qureshi <uq0803@gmail.com>
2023-01-27 21:00:34 +0530

    python urgent commit

--- za.py
+++ za.py
@@ -1,19 +1,19 @@
-# # # import calendar
-# # # for i in range(1,13):
-# # #     print(calendar.month(2023,i))
-# # # print(a.replace("country","India"))
-# # # print(a.upper())
-# # # print(a.isupper())
-# # # print(a.lower())
-# # # print(a.islower())
-# # # print(a.capitalize())
-# # # print("Hello ,%s"%a)
+# import calendar
+# for i in range(1,13):
+#     print(calendar.month(2023,i))
+# print(a.replace("country","India"))
+# print(a.upper())
+# print(a.isupper())
+# print(a.lower())
+# print(a.islower())
+# print(a.capitalize())
+# print("Hello ,%s"%a)
 
 
 # # a="9890562214"
 # # print(f"The value of a is : {a}")
 # a="Python Programming"
 # print(a.split(' '))
-str="Python Programming"
-print(len(str))
-print(str[::-1])
+# str="Python Programming"
+# print(len(str))
+# print(str[::-1])
