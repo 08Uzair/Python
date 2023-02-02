27c227f6d19b5cb56fc3a2f09e35675ee5e18c67
Uzer Qureshi <uq0803@gmail.com>
2023-02-02 09:15:58 +0530

    24

--- test.py
+++ test.py
@@ -1,16 +1,60 @@
-# a="I love Python Programming"
-# b=(len(a))
-# i=1
-# while i<=1:
-#     print(a[::-1])
-#     i=i+1;
-# a="Python"
-# b=(len(a))
-# print(a[-3:])
-str="Python"
-new_str=" "
-print(str[-1])
-new_str=str[-1]
-print(new_str)
-new_str=str[-2]
-print(new_str)
+# # # # # a="I love Python Programming"
+# # # # # b=(len(a))
+# # # # # i=1
+# # # # # while i<=1:
+# # # # #     print(a[::-1])
+# # # # #     i=i+1;
+# # # # # a="Python"
+# # # # # b=(len(a))
+# # # # # print(a[-3:])
+# # # # # str="Python"
+# # # # # new_str=" "
+# # # # # print(str[-1])
+# # # # # new_str=str[-1]
+# # # # # print(new_str)
+# # # # # new_str=str[-2]
+# # # # # print(new_str)
+
+# # # # a="I"
+# # # # b="am"
+# # # # c="Uzer"
+# # # # print(a+b+c)
+# # # STR="Pyhton Programming"
+# # # print(len(STR))
+# # # print(STR)
+# # # print(STR.rjust(22,'&'))
+# # str=("Python Programming")
+# # new=(list(str))
+
+# # print(type(new))
+
+
+
+
+
+# str=input("Enter a String :\n ")
+# palin=str[::-1] #tO check wearher the given string is palindrome or not ::
+# if (list(str)==list(palin)):
+#     print("The Given String is the Palindrome")
+# else:
+#     print("The given String is not the Palindrome ")
+
+
+
+
+    
+
+
+
+
+
+list=('a','b')
+print(type(list))
+
+
+
+
+
+
+
+
