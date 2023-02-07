707222016cc98e7df03597f3168a5ab161f0cdc3
Uzer Qureshi <uq0803@gmail.com>
2023-02-07 20:57:19 +0530

    789

--- try1.py
+++ try1.py
@@ -1,42 +1,75 @@
-# # # a="aaa"
-# # # b="AAA"
-# # # print(a<b)
-# # # # in Python dictionary the lower case is greater than upper case
+# # # # a="aaa"
+# # # # b="AAA"
+# # # # print(a<b)
+# # # # # in Python dictionary the lower case is greater than upper case
 
-# # # str="  python "
-# # # msg=str.capitalize()
-# # # print(msg)
+# # # # str="  python "
+# # # # msg=str.capitalize()
+# # # # print(msg)
 
 
-# # str="1234"
-# # print(str.isdigit())
+# # # str="1234"
+# # # print(str.isdigit())
 
-# # a="abc124"
-# # coutalphabet=0
-# # coutdigit=0
-# # for i in a:
-# #     if i.isdigit():
-# #         coutdigit+=1
-# #     else:
-# #         coutalphabet+=1
-# # print(coutalphabet)
-# # print(coutdigit)
+# # # a="abc124"
+# # # coutalphabet=0
+# # # coutdigit=0
+# # # for i in a:
+# # #     if i.isdigit():
+# # #         coutdigit+=1
+# # #     else:
+# # #         coutalphabet+=1
+# # # print(coutalphabet)
+# # # print(coutdigit)
     
-# a="I Love IceCream"
-# c="tiger"
-# b=a.join(c)
-# print(b)
-# # b=(list(a))
-# # b.split()c
+# # a="I Love IceCream"
+# # c="tiger"
+# # b=a.join(c)
 # # print(b)
-
-# # a=10
-# # b=20
+# # # b=(list(a))
+# # # b.split()c
 # # # print(b)
-# # a-=b
-# # print(a)
 
-a="I Love IceCream"
-b=a.split()
-c=b.sort()
-print(f"THe list is sorted in order{c}")
+# # # a=10
+# # # b=20
+# # # # print(b)
+# # # a-=b
+# # # print(a)
+
+# a="I Love IceCream"
+# b=a.split()
+# c=b.sort()
+# print(f"THe list is sorted in order{c}")
+
+
+
+
+import trail.py
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
