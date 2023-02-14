3d83335c67cc3593f66cc1430fe5c078a22d963a
Uzer Qureshi <uq0803@gmail.com>
2023-02-14 11:27:18 +0530

    Uzer Qureshi

--- za.py
+++ za.py
@@ -1,31 +1,36 @@
-# import calendar
-# for i in range(1,13):
-#     print(calendar.month(2023,i))
-# print(a.replace("country","India"))
-# print(a.upper())
-# print(a.isupper())
-# print(a.lower())
-# print(a.islower())
-# print(a.capitalize())
-# print("Hello ,%s"%a)
+# # import calendar
+# # for i in range(1,13):
+# #     print(calendar.month(2023,i))
+# # print(a.replace("country","India"))
+# # print(a.upper())
+# # print(a.isupper())
+# # print(a.lower())
+# # print(a.islower())
+# # print(a.capitalize())
+# # print("Hello ,%s"%a)
 
 
-# # a="9890562214"
-# # print(f"The value of a is : {a}")
-# a="Python Programming"
-# print(a.split(' '))
-# str="Python Programming"
-# print(len(str))
-# print(str[::-1])
+# # # a="9890562214"
+# # # print(f"The value of a is : {a}")
+# # a="Python Programming"
+# # print(a.split(' '))
+# # str="Python Programming"
+# # print(len(str))
+# # print(str[::-1])
 
 
 
-from string import Template
-str=Template('$name is intresting')
-str1=str.substitute(name="Python")
-print(str)
-str=str.substitute(name='Harry')
-print(str)
+# from string import Template
+# str=Template('$name is intresting')
+# str1=str.substitute(name="Python")
+# print(str)
+# str=str.substitute(name='Harry')
+# print(str)
+
+
+
+
+
 
 
 
